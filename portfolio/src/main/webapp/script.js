94296dab9f74dd2a6eb7d3b23a6499f63d72986f
Nathan Safir <nssafir@gmail.com>
2020-06-01 13:59:59 -0500

    Complete first two steps of week 3 server tutorial.

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -24,3 +24,41 @@ function changeSettings() {
         links[i].style.color = "green";
     }
 }
+
+
+/**
+ * Fetches "Hello Nathan!" from the server and adds it to the DOM.
+ */
+function getName() {
+  console.log('Fetching name.');
+
+  // The fetch() function returns a Promise because the request is asynchronous.
+  const responsePromise = fetch('/data');
+
+  // When the request is complete, pass the response into handleResponse().
+  responsePromise.then(handleResponse);
+}
+
+/**
+ * Handles response by converting it to text and passing the result to
+ * addNameToDom().
+ */
+function handleResponse(response) {
+  console.log('Handling the response.');
+
+  // response.text() returns a Promise, because the response is a stream of
+  // content and not a simple variable.
+  const textPromise = response.text();
+
+  // When the response is converted to text, pass the result into the
+  // addQuoteToDom() function.
+  textPromise.then(addNameToDom);
+}
+
+/** Adds name to the DOM. */
+function addNameToDom(name) {
+  console.log('Adding name to dom: ' + name);
+
+  const nameContainer = document.getElementById('name-container');
+  nameContainer.innerText = name;
+}
